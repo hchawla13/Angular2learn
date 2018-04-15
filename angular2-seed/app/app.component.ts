0aa4534633535e49390806a92d3c493e18c2f9ff
US\hchawla <hchawla@deloitte.com>
2018-04-15 16:32:09 +0530

    using interpolation and for loop

--- angular2-seed/app/app.component.ts
+++ angular2-seed/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from 'angular2/core';
 import {CoursesComponent} from './courses.component';
 
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1><courses></courses>',
+    template: '<h1>My First Angular 2 App</h1><courses></courses><subject></subject>',
     directives :[CoursesComponent]
 })
 
--- angular2-seed/app/courses.component.ts
+++ angular2-seed/app/courses.component.ts
@@ -2,9 +2,16 @@ import {Component} from 'angular2/core';
 
 @Component({
 	selector:'courses',
-	template:'<h2>Courses</h2>'
+	template:`<h2>Courses</h2>
+	{{title}}
+	<ul>
+		<li *ngFor="#course of courselist">{{course}}</li>
+	</ul>
+
+	`
 })
 
 export class CoursesComponent {
-
+	title = "This is the title";
+	courselist = ["Course1","Course2","Course3","Course4"]
 }

e9573cecaf4b18adb9244b95add8073b713b7adc
US\hchawla <HarpreetSingh.Chawla@team.telstra.com>
2019-05-24 12:37:54 +0530

    01-develop

--- angular2-seed/app/app.component.ts
+++ angular2-seed/app/app.component.ts
@@ -1,14 +1,3 @@
-import {Component} from 'angular2/core';
-import {CoursesComponent} from './courses.component';
-import {AuthorComponent} from './author.component';
-
-
-@Component({
-    selector: 'my-app',
-    template: `<h1>My First Angular 2 App</h1><courses></courses><subject></subject>
-    			<author></author>
-    `,
-    directives :[CoursesComponent,AuthorComponent]
-})
-
-export class AppComponent { }
+01
+02
+stash-03
